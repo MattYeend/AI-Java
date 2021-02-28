f9139da0366029fc5df629b76134e4f5f7dcf282
Matthew Yeend <mattyeend1@hotmail.co.uk>
2021-02-28 11:30:56 +0000

    initial commit

---
+++ src/ai/AI.java
@@ -0,0 +1,27 @@
+/*
+    Title:      AI
+    Author:     Matthew Yeend
+    Date:       04/03/2017
+    Version:    1.0
+ */
+package ai;
+
+/**
+ * @author Matt
+ * @version 1.0
+ */
+
+import java.util.Scanner;
+
+public class AI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //AI number 1
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please ");
+    }
+    
+}
